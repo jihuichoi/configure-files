b6c9968c75013940b35a4fa32b26f46e6cbf7012
Jihui Choi <jihui.choi@gmail.com>
2019-09-20 16:56:07 +0900

    change 설정 항목이 존재하지 않을 경우, 추가하도록 변경

--- scripts/vlc-remote-control-setting.go
+++ scripts/vlc-remote-control-setting.go
@@ -18,15 +18,25 @@ func main() {
 	}
 }
 
-func changeConfig(keyword string, value string) {
-	cmd := "grep " + keyword + " " + VLCCONFIG
+// vlc 설정 변경
+func changeConfig(key string, value string) {
+
+	// 변경하려는 설정 항목이 있는지 확인
+	cmd := "grep " + key + " " + VLCCONFIG
 	out, err := exec.Command("/bin/sh", "-c", cmd).Output()
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// 있으면 바꾸고, 없으면 추가
 	if string(out[:]) != "" {
-		cmd = fmt.Sprintf("sed -ie 's/.%s.*/%s=%s/g' %s", keyword, keyword, value, VLCCONFIG)
+		cmd = fmt.Sprintf("sed -i 's/^#\\?%s.*/%s=%s/g' %s", key, key, value, VLCCONFIG)
+		_, err := exec.Command("/bin/sh", "-c", cmd).Output()
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		cmd = fmt.Sprintf("echo '\n# %s\n%s=%s' >> %s", key, key, value, VLCCONFIG)
 		_, err := exec.Command("/bin/sh", "-c", cmd).Output()
 		if err != nil {
 			fmt.Println(err)
